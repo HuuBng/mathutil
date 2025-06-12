13480c4e3369251a08506bbcf7eba14d05df2d48
HuuBng <89332259+HuuBng@users.noreply.github.com>
2025-06-12 17:08:36 +0700

    Update main code to use recursion and to see green 4ever

--- src/main/java/com/huubng/tool/mathutil/core/MathUtility.java
+++ src/main/java/com/huubng/tool/mathutil/core/MathUtility.java
@@ -10,23 +10,40 @@ public class MathUtility {
     // KO CÓ GIAI THỪA SỐ ÂM
     // 0! = 1
     // 20! VỪA ĐỦ KIỂU LONG, TỨC 21! VƯỢT KIỂU LONG
+//    public static long getFactorial(int n) {
+//        // ĐEM CODE CHÍNH RA SỬA GỌI LÀ REFACTOR/REFACTORING
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("n must be between 0 and 20");
+//        } // bạn đưa invalid data, tớ ném ra ngoại lệ, hàm ko đc chạy thành công, ko value trã về
+//
+//        if (n == 0)
+//            return 1;
+//
+//        // sống sót tới đây n = 1...19
+//
+//        long result = 1; // thuật toán dồn con heo đất
+//
+//        for (int i = 1; i <= n; i++)
+//            result *= i; // nhân dồn vào từng cháu i
+//
+//        return result;
+//    }
+
     public static long getFactorial(int n) {
         // ĐEM CODE CHÍNH RA SỬA GỌI LÀ REFACTOR/REFACTORING
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("n must be between 0 and 20");
         } // bạn đưa invalid data, tớ ném ra ngoại lệ, hàm ko đc chạy thành công, ko value trã về
 
-        if (n == 0)
+        if (n == 0 || n == 1)
             return 1;
 
-        // sống sót tới đây n = 1...19
-
-        long result = 1; // thuật toán dồn con heo đất
-
-        for (int i = 1; i <= n; i++)
-            result *= i; // nhân dồn vào từng cháu i
-
-        return result;
+        // ĐỆ QUY - RECURSION - GỌI LẠI CHÍNH MÌNH VỚI QUY MÔ NHỎ HƠN
+        return n + getFactorial(n-1);
     }
+    // ĐEN LÊN SERVER RA MÀU XANH
+        // 1. CODE TỐI ƯU NGON VÌ PASS TEST CASE
+        // 2. TEST LẠI TỪNG THỨ RẤT MẤT CÔNG NAY CÓ SCRIPT TEST TỰ ĐỘNG
+            // REGRESSION TESTNG KIỂM THỬ HỒI QUY
 
 }
